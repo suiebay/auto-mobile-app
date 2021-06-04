c82bf1cc5e65c685a23181cb3fbd265c7e6d68cc
zhassulan <suiebayzh@gmail.com>
2021-06-04 10:43:58 +0600

    hot-fix: bloc fixed

--- lib/logic/user/bloc/contacts_bloc.dart
+++ lib/logic/user/bloc/contacts_bloc.dart
@@ -1,21 +1,22 @@
 import 'package:auto_mobile_app/logic/user/domain/entities/user.dart';
 import 'package:auto_mobile_app/logic/user/domain/repositories/users_repository.dart';
+import 'package:auto_mobile_app/logic/user/domain/usecases/get_user.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'contacts_bloc_state.dart';
 part 'contacts_event.dart';
 
 class UsersBloc extends Bloc<UsersEvent, UsersState> {
-  final UsersRepository usersRepository;
+  final GetUser getUser;
 
-  UsersBloc(this.usersRepository) : super(UsersInitial());
+  UsersBloc(this.getUser) : super(UsersInitial());
 
   @override
   Stream<UsersState> mapEventToState(UsersEvent event) async* {
     if(event is UsersLoaded) {
       yield UsersLoading();
       try {
-        final user = await usersRepository.getUsers(event.id);
+        final user = await getUser(Params(number: event.id));
         yield user.fold(
             (failure) => UsersFailure(failure.toString()),
             (user) => UsersSuccess(user)
