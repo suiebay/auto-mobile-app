943ead573d5db24739e0ff0fc83b3c332bc86623
zhassulan <suiebayzh@gmail.com>
2021-06-05 09:48:09 +0600

    hot-fix: dio interceptor uncommented

--- lib/core/utils/dio/dio_interceptor.dart
+++ lib/core/utils/dio/dio_interceptor.dart
@@ -1,26 +1,26 @@
 import 'package:dio/dio.dart';
-// import 'package:hive/hive.dart';
+import 'package:hive/hive.dart';
 
 class DioInterceptor extends Interceptor {
-  // final Box? tokens;
-  // final Dio? dio;
-  //
-  // DioInterceptor({
-  //   this.tokens,
-  //   this.dio,
-  // });
-  //
-  // @override
-  // Future onRequest(
-  //     RequestOptions options, RequestInterceptorHandler handler) async {
-  //   String? accessToken = tokens?.get('access');
-  //
-  //   if (accessToken != null) {
-  //     if (!options.path.contains('signin')) {
-  //       options.headers['Authorization'] = 'Bearer $accessToken';
-  //     }
-  //   }
-  //
-  //   return handler.next(options);
-  // }
+  final Box? tokens;
+  final Dio? dio;
+
+  DioInterceptor({
+    this.tokens,
+    this.dio,
+  });
+
+  @override
+  Future onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    String? accessToken = tokens?.get('access');
+
+    if (accessToken != null) {
+      if (!options.path.contains('signin')) {
+        options.headers['Authorization'] = 'Bearer $accessToken';
+      }
+    }
+
+    return handler.next(options);
+  }
 }
